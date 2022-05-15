1d11622ec73899a2556d5a5e3e95ef64f8650361
sergiy-smaga <seregasmaga@gmail.com>
2022-05-15 13:49:09 +0300

    5th task fixed

--- js/task-05.js
+++ js/task-05.js
@@ -3,6 +3,9 @@ const spanRef = document.querySelector("#name-output");
 
 function inputHandler() {
     spanRef.textContent = inputRef.value
+    if (inputRef.value === "") {
+        spanRef.textContent = "Anonymous";
+    }
 };
 
 inputRef.addEventListener("input", inputHandler);
